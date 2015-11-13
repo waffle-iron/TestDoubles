5f52a7fe8bbbeb0323565b4f20d79f06573e95c4
Dhruv Mevada <mevadadhruv90@gmail.com>
2015-11-13 09:19:55 -0800

    fixed bug that hardcoded installation of td to vagrant

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "Downloading TD file from" $TD_HOST
 curl -O -s $TD_HOST/bin/td 
 sudo chmod +x td
 echo "set TD Path"
-sudo ln -sf /home/vagrant/td /usr/local/bin/td
+sudo ln -sf ./td /usr/local/bin/td
 #export PATH=$PATH:.
 sed -ie "1s@TD_HOST=.*@TD_HOST=\"${TD_HOST}\"@" td
 rm tde
